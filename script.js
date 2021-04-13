7fd2e0af786f469d18a284161e1eb60bf95ddb38
bsondermann <bastian.sondermann@web.de>
2021-04-14 01:39:02 +0200

    Update
    WAAY better design and basic input testing

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function convert(){
 	var str = document.getElementById("text1").value;
-	var converted = document.getElementById("name").value+": d=1,o=1,b="+document.getElementById("bpm").value+": ";
+	var name = document.getElementById("name").value;
+	var bpm = document.getElementById("bpm").value;
+	if(str == ""||name==""||bpm==""){return;}
+	if(bpm<1){document.getElementById("converted").innerHTML="ERROR: BPM HAS TO BE GREATER THAN 0"; return;}
+	if(/[h-z]/.test(str)){document.getElementById("converted").innerHTML="ERROR: INVALID INPUT"; return;}
+	var converted = name+": d=1,o=1,b="+bpm+": ";
 	var splited = str.split(" ");
 	for(var i = 0; i<splited.length; i++){
 		var note = splited[i].substring(0,1);
@@ -21,5 +26,8 @@ function convert(){
 		converted += length+note.toLowerCase()+octave+", "
 	}
 	converted = converted.substring(0,converted.length-2);
+	if (converted.search("undefined")!=-1){
+		document.getElementById("converted").innerHTML="ERROR: INVALID INPUT"; return;
+	}
 	document.getElementById("converted").innerHTML = converted;
 }
